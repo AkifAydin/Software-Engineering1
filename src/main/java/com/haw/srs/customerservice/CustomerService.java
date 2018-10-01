b99280a3fe13cc35b32d31e76459437d9986900a
Stefan Sarstedt <stefan.sarstedt@haw-hamburg.de>
2018-10-01 13:32:24 +0200

    added transaction support

--- src/main/java/com/haw/srs/customerservice/CustomerService.java
+++ src/main/java/com/haw/srs/customerservice/CustomerService.java
@@ -2,6 +2,7 @@ package com.haw.srs.customerservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,6 +20,7 @@ public class CustomerService {
         return customerRepository.findAll();
     }
 
+    @Transactional
     public void transferReservations(String fromCustomer, String toCustomer) throws CustomerNotFoundException {
 
 // Alternativ zu unten:
