90add9fb5d039ffe02c04040b2d4c5a344bd49b8
Arne Busch <arne.busch@haw-hamburg.de>
2020-12-26 22:59:00 +0100

    Added example for custom query using @Query

--- src/main/java/com/haw/se1lab/dataaccess/api/repo/CourseRepository.java
+++ src/main/java/com/haw/se1lab/dataaccess/api/repo/CourseRepository.java
@@ -3,6 +3,8 @@ package com.haw.se1lab.dataaccess.api.repo;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.haw.se1lab.dataaccess.api.entity.Course;
 
@@ -20,7 +22,7 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
 	 * @param name the course's name
 	 * @return an {@link Optional} containing the found course
 	 */
-	// @Query("TODO")
-	Optional<Course> findByName(String name);
+	@Query("select c from Course c where c.name = :name")
+	Optional<Course> findByName(@Param("name") String name);
 
 }
