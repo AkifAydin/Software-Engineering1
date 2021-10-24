8b7e97180963fa08a4c0fe892cfaa019800875ce
Arne Busch <arne.busch@haw-hamburg.de>
2021-10-24 13:40:54 +0200

    Added note not to move main application class

--- src/main/java/com/haw/se1lab/Application.java
+++ src/main/java/com/haw/se1lab/Application.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+	// IMPORTANT: This class must lie in the root application package, i.e. next to or above all other classes/packages
+	// Spring component scan only recognizes classes below main application class (the one with @SpringBootApplication)
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
