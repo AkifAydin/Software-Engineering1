cc82bfd1a247cb3ccce838100ca828b06afd524b
AkifAydin <akif.aydin@outlook.de>
2022-01-15 17:25:08 +0100

    4.2 Finished Impl. UserStory

--- src/main/java/com/haw/se1lab/todolist/logic/impl/usecase/TodoListUseCaseImpl.java
+++ src/main/java/com/haw/se1lab/todolist/logic/impl/usecase/TodoListUseCaseImpl.java
@@ -6,14 +6,17 @@ import com.haw.se1lab.todolist.logic.api.usecase.TodoListUseCase;
 import com.haw.se1lab.todolist.strategy.TodoListSortingStrategy;
 import com.haw.se1lab.user.dataaccess.api.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
+//Sorgt für Berücksichtigung beim Autowiring
+//Component can also be used
 @Service // causes Spring to automatically create a Spring bean for this class which can then be used using @Autowired
 public class TodoListUseCaseImpl implements TodoListUseCase {
 
+    //Field Injection durch Autowiring
     @Autowired // automatically initializes the field with a Spring bean of a matching type
     private TodoListRepository todoListRepository;
 
