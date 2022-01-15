d798602b31e868faf56332ab492451919a3099b9
MasterOC <benediktweyer@gmx.de>
2022-01-15 17:56:35 +0100

    added Querys UserRepository

--- src/main/java/com/haw/se1lab/user/dataaccess/api/repo/UserRepository.java
+++ src/main/java/com/haw/se1lab/user/dataaccess/api/repo/UserRepository.java
@@ -2,6 +2,7 @@ package com.haw.se1lab.user.dataaccess.api.repo;
 
 import com.haw.se1lab.user.common.api.datatype.UserIDTyp;
 import com.haw.se1lab.user.dataaccess.api.entity.User;
+import com.haw.se1lab.workgroup.common.api.datatype.WorkGroupIDTyp;
 import com.haw.se1lab.workgroup.dataaccess.api.entity.WorkGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 /**
  * Represents a repository for the management of {@link User} entities in a database.
@@ -16,6 +18,25 @@ import javax.transaction.Transactional;
  * @author Janat Haref, Benedikt Weyer, Akif Aydin
  */
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    /**
+     * Returns the {@link WorkGroup} entity with the given UserID.
+     *
+     * @param userId the user id
+     * @return an {@link Optional} containing the found user
+     */
+    @Query("select u from User u where u.id = :userId")
+    Optional<WorkGroup> findByUserIDTyp(@Param("userId") UserIDTyp userId);
+
+    /**
+     * Returns the {@link WorkGroup} entity with the given email and password.
+     *
+     * @param email the user email
+     * @param password the user password
+     * @return an {@link Optional} containing the found user
+     */
+    @Query("select u from User u where u.email = :email and u.password = :password")
+    Optional<WorkGroup> findByEmailAndPassword(@Param("email") String email, @Param("password") String password);
 
     /**
      * Deletes the {@link User} entity with the given UserID.
