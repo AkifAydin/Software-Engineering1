2f899454b2e30cef904324907746d7296c0803ab
MasterOC <benediktweyer@gmx.de>
2022-01-15 17:34:38 +0100

    4.3 Finished Impl. TodoList Facade

--- src/main/java/com/haw/se1lab/todolist/facade/impl/TodoListFacadeImpl.java
+++ src/main/java/com/haw/se1lab/todolist/facade/impl/TodoListFacadeImpl.java
@@ -1,4 +1,22 @@
 package com.haw.se1lab.todolist.facade.impl;
 
-public class TodoListFacadeImpl {
+import com.haw.se1lab.todolist.dataaccess.api.entity.TodoList;
+import com.haw.se1lab.todolist.facade.api.TodoListFacade;
+import com.haw.se1lab.todolist.logic.api.usecase.TodoListUseCase;
+import com.haw.se1lab.user.dataaccess.api.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class TodoListFacadeImpl implements TodoListFacade {
+
+    @Autowired
+    private TodoListUseCase todoListUseCase;
+
+    @Override
+    public List<TodoList> getAllTodoListsFromUser(User user) {
+        return todoListUseCase.findAllTodoListsFromUser(user);
+    }
 }
