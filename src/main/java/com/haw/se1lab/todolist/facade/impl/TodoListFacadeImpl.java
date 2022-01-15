27de734359126ee94b11041cfb02957781c61838
MasterOC <benediktweyer@gmx.de>
2022-01-15 17:43:13 +0100

    4.3.b Finished Impl. TodoList REST

--- src/main/java/com/haw/se1lab/todolist/facade/impl/TodoListFacadeImpl.java
+++ src/main/java/com/haw/se1lab/todolist/facade/impl/TodoListFacadeImpl.java
@@ -6,17 +6,24 @@ import com.haw.se1lab.todolist.logic.api.usecase.TodoListUseCase;
 import com.haw.se1lab.user.dataaccess.api.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @Component
+@RestController
+@RequestMapping(path = "/todolist")
 public class TodoListFacadeImpl implements TodoListFacade {
 
     @Autowired
     private TodoListUseCase todoListUseCase;
 
+    @PostMapping
     @Override
-    public List<TodoList> getAllTodoListsFromUser(User user) {
+    public List<TodoList> getAllTodoListsFromUser(@RequestBody User user) {
         return todoListUseCase.findAllTodoListsFromUser(user);
     }
 }
